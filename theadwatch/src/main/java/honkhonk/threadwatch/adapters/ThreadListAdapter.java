e6f60fc95ca77b7948b2ccc45ababf9d70895363
Gunbard <gunbard@gmail.com>
2016-10-29 00:31:50 -0500

    asdfasdfsadf

--- theadwatch/src/main/java/honkhonk/threadwatch/adapters/ThreadListAdapter.java
+++ theadwatch/src/main/java/honkhonk/threadwatch/adapters/ThreadListAdapter.java
@@ -5,10 +5,12 @@ import android.support.annotation.IdRes;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -16,11 +18,24 @@ import honkhonk.threadwatch.R;
 import honkhonk.threadwatch.models.ThreadModel;
 
 /**
+ * Handles display of threads in the list
  * Created by Gunbard on 10/15/2016.
  */
 
 public class ThreadListAdapter extends ArrayAdapter<ThreadModel> {
+    /**
+     * Tag for logging
+     */
+    final private static String TAG = ThreadListAdapter.class.getSimpleName();
+
+    /**
+     * List of threads
+     */
     private ArrayList<ThreadModel> threads;
+
+    /**
+     * Context for getting resources
+     */
     private Context context;
 
     public ThreadListAdapter(Context context, @LayoutRes int resource, @IdRes int textViewResourceId,
@@ -35,38 +50,50 @@ public class ThreadListAdapter extends ArrayAdapter<ThreadModel> {
     public View getView(int position, View convertView, @NonNull ViewGroup parent) {
         final View view = super.getView(position, convertView, parent);
 
-        if (position < threads.size()) {
-            final ThreadModel thread = threads.get(position);
-
-            final TextView boardName = (TextView) view.findViewById(R.id.boardTitle);
-            boardName.setText("/" + thread.board + "/");
-
-            final TextView title = (TextView) view.findViewById(R.id.threadTitle);
-            final String titleText = thread.getTitle();
-
-            if (titleText != null && !titleText.equals("")) {
-                title.setText(thread.getTitle());
-            } else {
-                title.setText(context.getResources().getString(R.string.no_thread_data));
-            }
-
-            final TextView newPosts = (TextView) view.findViewById(R.id.newPosts);
-            if (thread.replyCountDelta != 0) {
-                newPosts.setText("(" + thread.replyCountDelta + ")") ;
-            } else {
-                newPosts.setText("");
-            }
-
-            if (thread.archived) {
-                view.setBackgroundColor(ContextCompat.getColor(context, R.color.colorArchivedThread));
-            } else if (thread.closed) {
-                view.setBackgroundColor(ContextCompat.getColor(context, R.color.colorClosedThread));
-            } else if (thread.notFound) {
-                view.setBackgroundColor(ContextCompat.getColor(context, R.color.colorNotFoundThread));
-            } else {
-                view.setBackgroundColor(ContextCompat.getColor(context, R.color.colorThreadBackground));
-            }
+        if (position >= threads.size()) {
+            Log.e(TAG, "Tried to show a thread outside of bounds of data source");
+            return view;
         }
+
+        final ThreadModel thread = threads.get(position);
+
+        final TextView boardName = (TextView) view.findViewById(R.id.boardTitle);
+        boardName.setText("/" + thread.board + "/");
+
+        final TextView title = (TextView) view.findViewById(R.id.threadTitle);
+        final String titleText = thread.getTitle();
+
+        if (titleText != null && !titleText.equals("") && !thread.firstRefresh) {
+            title.setText(thread.getTitle());
+        } else {
+            title.setText(context.getResources().getString(R.string.no_thread_data));
+        }
+
+        final TextView newPosts = (TextView) view.findViewById(R.id.newPosts);
+        if (thread.replyCountDelta != 0) {
+            newPosts.setText("(" + thread.replyCountDelta + ")") ;
+        } else {
+            newPosts.setText("");
+        }
+
+        if (thread.archived) {
+            view.setBackgroundColor(ContextCompat.getColor(context, R.color.colorArchivedThread));
+        } else if (thread.closed) {
+            view.setBackgroundColor(ContextCompat.getColor(context, R.color.colorClosedThread));
+        } else if (thread.notFound) {
+            view.setBackgroundColor(ContextCompat.getColor(context, R.color.colorNotFoundThread));
+        } else {
+            view.setBackgroundColor(ContextCompat.getColor(context, R.color.colorThreadBackground));
+        }
+
+        // Set up preview event
+        boardName.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(getContext(), boardName.getText(), Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
 
         return view;
     }
